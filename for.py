69ee614f5dd7dae14f75ba10a8fc3a80ba588cf0
WesleyB12 <lombard+wesleyb@thecoderschool.com>
2026-01-17 11:25:47 -0600

    loops practice

--- for.py
+++ for.py
@@ -1,2 +1,23 @@
-for i in range (10):
-    print (i)
+# a = int(input())
+
+# if a > 0 and a < 10:
+#     print(a, "is grater than 0 and less thN 10")
+# elif a < 0 and a > -10:
+#     print(a, "is less than 0 and greater than -10")
+# else:
+#     print(a)
+# a = 1
+# while a != 0:
+#     a = int(input())
+#     print("a is ", a)
+# print("loop finished")
+
+a = ["a", 1, 4, "s"]
+# print(a)
+# for i in range(len(a)):
+#     print(a[i], "is at index", i)
+# for each
+# ranged for loop
+
+for i in a:
+    print(i)
---
+++ loopsPractice.py
@@ -0,0 +1,24 @@
+# 1. we have a dictionary x, and we want to print out every value from x
+# 2. we want to grab input from user. and keep counting how many times we've grabbed input. until the user typed 'quit'quit
+# 3. we want to grab input and check whehter it is even or odd
+# 4. we want to print out every other element from dictionary x
+
+# For Each loop 
+# Ranged For loop
+# while loop
+# if statement
+
+x =[1, 2, 3, 4, 5]
+#  0  1  2  3  4
+
+# print(x)
+# for i in range(len(x)):
+#     x[i] = (i + 2)
+
+# print(x)
+
+print(x)
+for i in x:
+    i = 0
+print(x)
+# [0, 1, 2, 3, 4]
