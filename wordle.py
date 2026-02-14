cce7f75a777945596057605e8b6d07ad233d0ead
malahdab8603-crypto <malahdab8603@stu.hinsdale86.org>
2026-02-14 13:00:57 -0600

    progress

--- wordle.py
+++ wordle.py
@@ -1,4 +1,13 @@
-word = "Rubics"
+from colorama import init, Fore, Style
+from random_word import RandomWords
+
+init(autoreset=True)
+
+r = RandomWords()
+
+word = r.get_random_word()
+GREEN = '\033[32m'
+RESET = '\033[0m'
 
 def grayLetter(letter, word, guess):
     if letter in word:
@@ -31,25 +40,42 @@ def greenLetter(letter, word, guess):
 def checkGuess(guess, word):
     result = []
     for letter in guess:
-        if grayLetter(letter, word, guess):
-            result.append("gray")
+        if greenLetter(letter, word, guess):
+            result.append(("green", letter))
         elif yellowLetter(letter, word, guess):
-            result.append("yellow")
-        elif greenLetter(letter, word, guess):
-            result.append("green")
+            result.append(("yellow", letter))
+        elif grayLetter(letter, word, guess):
+            result.append(("gray", letter))
     return result
 
+def printResult(result):
+    color_map = {
+        "green": Fore.GREEN,
+        "yellow": Fore.YELLOW,
+        "gray": Fore.BLACK
+    }
+    for color, letter in result:
+        print(color_map[color] + letter.upper(), end=" ")
+    print()
+
 def game():
-    while True:
-        guess = input("Enter your guess: ")
+    count = 0
+    guess = 5
+    while count < guess:
+        guess = input("Enter your guess: ").lower()
         if len(guess) != len(word):
             print(f"Please enter a {len(word)}-letter word.")
             continue
         result = checkGuess(guess, word)
-        print(result)
-        if all(color == "green" for color in result):
+        printResult(result)
+        colors = [color for color, _ in result]
+        if all(color == "green" for color in colors):
             print("Congratulations! You've guessed the word!")
             break
 
+        count+=1
+        print("Guesses so far: ", count)
+
 if __name__ == "__main__":
     game()
+
